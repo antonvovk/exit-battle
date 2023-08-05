dac345a3b04e9b9cf9a868e0fba0ec180717d314
Anton Vovk <archwolf@protonmail.com>
2023-08-05 19:15:04 +0300

    Initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'exit-battle'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('exit-battle');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('exit-battle app is running!');
+  });
+});
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'exit-battle',
+    appId: '1:733588597227:web:dc620221e59d6910b491ae',
+    storageBucket: 'exit-battle.appspot.com',
+    apiKey: 'AIzaSyBNKS3UXuxKbmhvOoyGJxBnJprucYcUj5s',
+    authDomain: 'exit-battle.firebaseapp.com',
+    messagingSenderId: '733588597227',
+    measurementId: 'G-28RJ07XTBW',
+  },
+};
