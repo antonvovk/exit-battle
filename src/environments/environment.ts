75f46e75350a52631ace5490f1a7a62f53b5a1a3
Anton Vovk <archwolf@protonmail.com>
2024-08-25 14:28:39 +0300

    chore: Update firebase configs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
     projectId: 'exit-battle',
-    appId: '1:733588597227:web:dc620221e59d6910b491ae',
-    storageBucket: 'exit-battle.appspot.com',
+    appId: '1:733588597227:web:0edc922d5bd3db87b491ae',
+    storageBucket: 'exit-battle-season2',
     apiKey: 'AIzaSyBNKS3UXuxKbmhvOoyGJxBnJprucYcUj5s',
     authDomain: 'exit-battle.firebaseapp.com',
     messagingSenderId: '733588597227',
-    measurementId: 'G-28RJ07XTBW',
+    measurementId: 'G-6RWDJEMRQZ',
   },
 };
